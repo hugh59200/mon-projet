071e8feb522e4f568502cc759981700ec3a7c0fd
Hugo BOGRAND <hugo.bogrand-ext@akto.fr>
2025-10-14 19:47:30 +0200

    commence auth

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,12 @@
 <template>
-  <SideBar />
   <main>
     <HeaderApp />
     <RouterView />
-    <FooterApp />
   </main>
-  <AppRegisterGlobals />
 </template>
 
 <script setup lang="ts">
   import HeaderApp from '@/features/interface/layout/header/HeaderApp.vue'
-  import SideBar from '@/features/interface/layout/sideBar/SideBar.vue'
-  import AppRegisterGlobals from './AppRegisterGlobals.vue'
-  import FooterApp from './features/interface/layout/footer/FooterApp.vue'
 </script>
 
 <style scoped lang="less">
